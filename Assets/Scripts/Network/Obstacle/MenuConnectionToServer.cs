4cb40a261e2cdb88d4899eb73d37d98e92e27879
Алексей Парников <cyraxchel@list.ru>
2023-12-04 00:04:29 +0300

    подготовка подключения к серверу без лобби.

---
+++ Assets/Scripts/Network/Obstacle/MenuConnectionToServer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuConnectionToServer : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text infoText;
+
+    [SerializeField]
+    Button cancelButton;
+
+    [SerializeField]
+    GameObject ConnectionPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayNetworkGame() {
+        //TODO
+    }
+}
