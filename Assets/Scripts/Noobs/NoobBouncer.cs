1ee86c5ff8589d46d369f09b81ca9339e2760f19
андрей огребо <ogreboandrey@yandex.ru>
2023-12-09 20:15:09 +0300

    боунсеры издают звук локально

--- Assets/Scripts/Noobs/NoobBouncer.cs
+++ Assets/Scripts/Noobs/NoobBouncer.cs
@@ -1,29 +1,33 @@
-using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoobBouncer : NetworkBehaviour
+public class NoobBouncer : MonoBehaviour
 {
     [SerializeField]
     private float _jumpHeight = 15;
     [SerializeField]
     private Animator _animator;
     [SerializeField]
+    private AudioClip _jumper;
+    [SerializeField]
     private GameObject _audioSoursePrefab;
+
+    private Queue<GameObject> _audioSources = new Queue<GameObject>();
+    [SerializeField]
+    private float _despawnTime = 2;
+    [SerializeField]
+    private bool _isDespawning;
 
     void Start()
     {
-    }
-    public override void OnStartLocalPlayer()
-    {
-        Debug.Log($"NoobBouncer: isLocalPlayer: {isLocalPlayer} isClient: {isClient} isServer: {isServer}");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,11 +36,18 @@ public class NoobBouncer : NetworkBehaviour
 
         if (other.tag.Equals("Player"))
         {
-            if(_audioSoursePrefab != null)
+            if (_audioSoursePrefab != null)
             {
-                if (isClient || isLocalPlayer)
+                var go = Instantiate(_audioSoursePrefab, transform);
+
+                var audio = go.GetComponent<AudioSource>();
+                audio.clip = _jumper;
+                audio.Play();
+                _audioSources.Enqueue(go);
+
+                if (!_isDespawning)
                 {
-                    CmdSpawnAudio();
+                    StartCoroutine(DespawnAudio());
                 }
             }
 
@@ -52,25 +63,41 @@ public class NoobBouncer : NetworkBehaviour
         }
     }
 
-    [Command]
-    void CmdSpawnAudio()
-    {
-        GameObject bulletClone = Instantiate(_audioSoursePrefab, transform.position, Quaternion.identity);
-        NetworkServer.Spawn(bulletClone);
-    }
-
     private void OnTriggerExit(Collider other)
     {
         _animator.SetBool("Jump", false);
     }
 
+    IEnumerator DespawnAudio()
+    {
+        _isDespawning = true;
+
+        while (_audioSources.Count > 0)
+        {
+            yield return new WaitForSeconds(_despawnTime);
+            Destroy(_audioSources.Dequeue());
+        }
+
+        _isDespawning = false;
+    }
 
     private void OnDestroy()
     {
+        DeactivateCouroutines();
     }
 
     private void OnDisable()
     {
+        DeactivateCouroutines();
     }
 
+    private void DeactivateCouroutines()
+    {
+        StopAllCoroutines();
+
+        while (_audioSources.Count > 0)
+        {
+            Destroy(_audioSources.Dequeue());
+        }
+    }
 }
