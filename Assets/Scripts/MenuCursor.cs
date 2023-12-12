c76a79a35d936a7183ecc40c657c587a78398db6
андрей огребо <ogreboandrey@yandex.ru>
2023-12-12 12:42:05 +0300

    правочки

---
+++ Assets/Scripts/MenuCursor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCursor : MonoBehaviour
+{
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        
+    }
+}
