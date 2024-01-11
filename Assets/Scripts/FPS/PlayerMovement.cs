0905aef6a04305132ac0d9d7ee24e5ccbc18bedd
cyraxchel <alexey-parnikov@mail.ru>
2024-01-11 17:39:45 +0300

    AI bot testing

--- Assets/Scripts/FPS/PlayerMovement.cs
+++ Assets/Scripts/FPS/PlayerMovement.cs
@@ -3,53 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public abstract class PlayerMovement : MonoBehaviour
-{
+namespace noobs {
+    public abstract class PlayerMovement : MonoBehaviour {
 
-    // Start is called before the first frame update
-    public virtual void Start()
-    {
-        Init(PlayerController.Instance.Character);        
-    }
-
-    // Update is called once per frame
-    public virtual void Update()
-    {
-        
-    }
-
-    public virtual void Init(CharacterController character)
-    {
-
-    }
-
-    public virtual void Switch()
-    {
-
-    }
-
-    public virtual void MoveValue(Vector2 inputMovement)
-    {
-
-    }
-
-    public virtual void JumpValue()
-    {
-
-    }
-
-    public virtual void RunValue(InputAction.CallbackContext value)
-    {
-        //Debug.Log("sprint");
-        if (value.ReadValue<float>() == 1)
-        {
-            Debug.Log("sprint");
+        // Start is called before the first frame update
+        public virtual void Start() {
+            Init(PlayerController.Instance.Character);
         }
-        else
-        {
-            Debug.Log("walk");
-        }
-        
 
+        // Update is called once per frame
+        public virtual void Update() {
+
+        }
+
+        public virtual void Init(CharacterController character) {
+
+        }
+
+        public virtual void Switch() {
+
+        }
+
+        public virtual void MoveValue(Vector2 inputMovement) {
+
+        }
+
+        public virtual void JumpValue() {
+
+        }
+
+        public virtual void RunValue(InputAction.CallbackContext value) {
+            //Debug.Log("sprint");
+            if (value.ReadValue<float>() == 1) {
+                Debug.Log("sprint");
+            } else {
+                Debug.Log("walk");
+            }
+
+
+        }
     }
 }
